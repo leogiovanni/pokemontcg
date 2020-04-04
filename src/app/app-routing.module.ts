c65efbfbf58e30c44c805be6c0da95a9737c45b0
Leonardo Augusto Giovanni <resource.lgiovanni@bv.com.br>
2020-04-04 10:39:15 -0300

    Create page skeleton and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',           component: PokemonListComponent },
+  { path: 'detail/:id', component: PokemonDetailComponent },
+  { path: "**",         component: PageNotFoundComponent},  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    PokemonDetailComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
