05cb290757310ad2889f5ffc77f80bbc2ca4e7f6
Leonardo Augusto Giovanni <resource.lgiovanni@bv.com.br>
2020-04-05 15:41:38 -0300

    Order list by name

--- src/app/pokemon-list/pokemon-list.filter.component.ts
+++ src/app/pokemon-list/pokemon-list.filter.component.ts
@@ -1,21 +1,35 @@
 import { Component, Pipe, PipeTransform } from '@angular/core';
 import { PokemonListComponent } from './pokemon-list.component';
 
+export type SortOrder = 'asc' | 'desc';
+
 @Pipe({  
     name: 'myfilter',  
     pure: false  
 })  
-  
+
 export class MyFilterPipe implements PipeTransform {  
 
-    transform(list: any[], filter: any): any {  
-        if (!list || !filter || filter.toString().length < 2) {  
-            return list;  
+    transform(list: any[], filter: any, sortOrder: SortOrder | string = 'asc', sortKey?: string): any { 
+        
+        let stringArray = [];
+
+        if(list){
+            stringArray = list.filter(item => typeof item[sortKey] === 'string').sort((a, b) => {
+                if (a[sortKey] < b[sortKey]) return -1;
+                else if (a[sortKey] > b[sortKey]) return 1;
+                else return 0;
+            });
+        }
+        
+        if (!filter || filter.toString().length < 2) {  
+            return stringArray;  
         }  
-        let filteredList = list.filter((item) => {
+        let filteredList = stringArray.filter((item) => {
             var name = item.name.toString().toLowerCase().indexOf(filter.toString().toLowerCase()) !== -1;
             return name;
         });
+
         return filteredList;
     }  
 } 
